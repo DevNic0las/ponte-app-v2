79833d6d36f284664d3f70e6844c7d81bd8c66b2
DevNic0las <nicolascajueiro098@gmail.com>
2026-04-10 19:43:09 -0300

    feat(core): add routes of pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/auth/auth.page').then(m => m.AuthPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./features/profile/profile.page').then(m => m.ProfilePage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.page').then(m => m.DashboardPage)
+  }
 ];
