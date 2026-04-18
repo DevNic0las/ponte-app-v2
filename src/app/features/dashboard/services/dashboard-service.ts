84446d7809cf02adb7ef4b6fa1e837c7edb369ec
DevNic0las <nicolascajueiro098@gmail.com>
2026-04-18 16:22:55 -0300

    feat(dashboard): integrate ticket count from API via DashboardService

---
+++ src/app/features/dashboard/services/dashboard-service.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { TicketCount } from '../models/dashboard-home/ticket-count.interface';
+@Injectable({
+  providedIn: 'root',
+})
+
+export class DashboardService {
+  private readonly prefix = 'ticket';
+
+  private readonly apiUrl = 'http://localhost:8080/api';
+
+  private readonly http: HttpClient = inject(HttpClient);
+
+  countTicketsByStatus(): Observable<TicketCount> {
+    return this.http.get<TicketCount>(`${this.apiUrl}/${this.prefix}/summary`)
+  }
+}
