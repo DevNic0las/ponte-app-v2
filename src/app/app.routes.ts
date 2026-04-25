fc49efa7c1e31b9337860e7f55f155700eb41fa2
DevNic0las <nicolascajueiro098@gmail.com>
2026-04-24 22:31:24 -0300

    feat(dashboard): add create ticket page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,6 +25,5 @@ export const routes: Routes = [
     path: 'dashboard',
     loadChildren: () =>
       import('./features/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
-    canActivate: [authGuard],
   },
 ];
