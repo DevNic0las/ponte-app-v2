d560b79aed87af6c6ed1d7565c277dfa39ef6586
Matheusssxz <theusnunes951@gmail.com>
2026-05-10 20:42:53 -0300

    feat(home): adição da pagina html, ts e scss do user menu, ajuste de routes e correções de imports standalone

--- src/app/features/home/user.menu.page.spec.ts
+++ src/app/features/home/user.menu.page.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HomePage } from './home.page';
+import { HomePage } from './user.menu.page';
 
 describe('HomePage', () => {
   let component: HomePage;
